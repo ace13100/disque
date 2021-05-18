31a4db1996b9555561c355968408befa90e53ea1
saadi <sadly4012@gmail.com>
2021-05-18 13:39:27 +0200

    changement dans disque.sql

---
+++ data.sql
@@ -0,0 +1,279 @@
+
+INSERT INTO Titre(titre) VALUES
+('Odeur de souffre'),
+('Odeur de souffre'),
+('Petit frère'),
+('Petit frère'),
+('Les jeunes de l\univers'),
+('L\enfant seul'),
+('Rêves illimités'),
+('Lose yourself'),
+('Lose yourself'),
+('Lose yourself'),
+('Still'),
+('Still'),
+('Kaneda'),
+('Teinshou'),
+('Blade runner - Main Titles'),
+('Blade runner - Blush response'),
+('Blade runner - Wait for me'),
+('Blade runner - Rachel song'),
+('Blade runner - Blade runner blues'),
+('Da funk'),
+('Da funk'),
+('Doing it right'),
+('Doing it right'),
+('Doing it right'),
+('Aerodynamic'),
+('Aerodynamic'),
+('older'),
+('older'),
+('older'),
+('older'),
+('older'),
+('overnight'),
+('overnight'),
+('overnight'),
+('overnight'),
+('overnight'),
+('Gamesofluck '),
+('Gamesofluck '),
+('Gamesofluck '),
+('Gamesofluck '),
+('Gamesofluck '),
+('Tieduprightnow '),
+('Tieduprightnow '),
+('Tieduprightnow '),
+('Tieduprightnow '),
+('Tieduprightnow '),
+('nightcall'),
+('nightcall'),
+('Shine On You Crazy Diamond'),
+('Shine On You Crazy Diamond'),
+('Shine On You Crazy Diamond'),
+('Welcome to the machine'),
+('Welcome to the machine'),
+('Welcome to the machine'),
+('Have a cigar'),
+('Have a cigar'),
+('Have a cigar'),
+('Wish you were here'),
+('Wish you were here'),
+('Wish you were here'),
+('bohemian rapsody'),
+('Cowboys'),
+('Cowboys'),
+('Cowboys'),
+('All mine'),
+('All mine'),
+('All mine'),
+('Big calm'),
+('The sea'),
+('Danse de la fée Dragée'),
+('Toccata et fugue'),
+('It\s probably me'),
+('It\s probably me'),
+('It\s probably me'),
+('English man in New York'),
+('If You Love Somebody Set Them Free'),
+('Walking on the moon'),
+('Chan chan'),
+('De Camino a La Vereda'),
+('El Cuarto de Tula'),
+('El Cuarto de Tula'),
+('Pueblo Nuevo'),
+('Pueblo Nuevo'),
+('Dos Gardenias'),
+('¿Y Tú qué has Hecho?'),
+('Veinte Años'),
+('El Carretero');
+
+
+INSERT INTO Auteur(nom) VALUES
+
+('Kool Shen'),
+('Joe Star'),
+('Akhenaton '),
+('Shurik\'n '),
+('Rocca'),
+('Oxmo Puccino'),
+('Casey'),
+('Eminem '),
+('Jeff Bass '),
+(' Luis Resto '),
+('Snoop Dogg '),
+('Dr.Dre '),
+('Geinoh Yamashirogumi '),
+('Geinoh Yamashirogumi '),
+('Vangelis'),
+('Vangelis'),
+('Vangelis'),
+('Vangelis'),
+('Vangelis'),
+('Thomas Bangalter '),
+('Guy-Manuel de Homem-Christo '),
+('Thomas Bangalter '),
+('Guy-Manuel de Homem-Christo '),
+('Panda Bear '),
+('Thomas Bangalter '),
+('Guy-Manuel de Homem-Christo '),
+('Jules Crommelin '),
+('Anatole Serret '),
+('Louie Swain '),
+('Noah Hill '),
+('Patrick Hetherington '),
+('Jules Crommelin '),
+('Anatole Serret '),
+('Louie Swain '),
+('Noah Hill '),
+('Patrick Hetherington '),
+('Jules Crommelin '),
+('Anatole Serret '),
+('Louie Swain '),
+('Noah Hill '),
+('Patrick Hetherington '),
+('Jules Crommelin '),
+('Anatole Serret '),
+('Louie Swain '),
+('Noah Hill '),
+('Patrick Hetherington '),
+('Kavinsky '),
+('Guy-Manuel de Homem-Christo '),
+('David Gilmour '),
+('Richard Wright '),
+('Roger Waters '),
+('David Gilmour '),
+('Richard Wright '),
+('Roger Waters '),
+('David Gilmour '),
+('Richard Wright '),
+('Roger Waters '),
+('David Gilmour '),
+('Richard Wright '),
+('Roger Waters '),
+('Freddie Mercury '),
+('Adrian Utley '),
+('Beth Gibbons '),
+('Geoff Barrow '),
+('Adrian Utley '),
+('Beth Gibbons '),
+('Geoff Barrow '),
+('Morcheeba'),
+('Morcheeba'),
+('Tchaïkovsky '),
+('Jean Sebastien Bach'),
+('Eric Clapton '),
+('Michael Kamen '),
+('Sting '),
+('Sting '),
+('Sting '),
+('Sting '),
+('Compay Segundo '),
+('Ibrahim Ferrer '),
+('Luis Marquetti '),
+('Sergio González Siaba '),
+('Ruben Gonzalez '),
+('Israel Cachao López '),
+('Isolina Carrillo '),
+('Eusebio Delfín '),
+('María Teresa Vera '),
+('Guillermo Portabales ');
+
+
+
+
+
+
+
+
+
+
+
+INSERT INTO Auteur(nom) VALUES
+('rap')
+('rap')
+('rap')
+('rap')
+('rap')
+('rap')
+('rap')
+('rap')
+('rap')
+('rap')
+('rap')
+('rap')
+('film')
+('film')
+('film')
+('film')
+('film')
+('film')
+('film')
+('electronique')
+('electronique')
+('electronique')
+('electronique')
+('electronique')
+('electronique')
+('electronique')
+('pop')
+('pop')
+('pop')
+('pop')
+('pop')
+('pop')
+('pop')
+('pop')
+('pop')
+('pop')
+('pop')
+('pop')
+('pop')
+('pop')
+('pop')
+('pop')
+('pop')
+('pop')
+('pop')
+('pop')
+('electronique')
+('electronique')
+('rock')
+('rock')
+('rock')
+('rock')
+('rock')
+('rock')
+('rock')
+('rock')
+('rock')
+('rock')
+('rock')
+('rock')
+('rock')
+('trip hop')
+('trip hop')
+('trip hop')
+('trip hop')
+('trip hop')
+('trip hop')
+('trip hop')
+('trip hop')
+('classique')
+('classique')
+('rock')
+('rock')
+('rock')
+('pop')
+('pop')
+('pop')
+('Salsa')
+('Salsa')
+('Salsa')
+('Salsa')
+('Salsa')
+('Salsa')
+('Salsa')
+('Salsa')
+('Salsa')
+('Salsa')
--- disque.sql
+++ disque.sql
@@ -1,28 +1,35 @@
 -- avant de faire le use music, il aurait fallu créer la BDD ici
+
 create database music;
 use music;
 CREATE TABLE Auteur(
     idAuteur INT NOT NULL AUTO_INCREMENT,
     nom TEXT NOT NULL,
     PRIMARY KEY (idAuteur)
-); -- pense à mettre le moteur de chaque table, sinon il prend le moteur défaut et si c'est MYISAM, alors les FK ne marcheront pas
+)ENGINE=InnoDB;-- pense à mettre le moteur de chaque table, sinon il prend le moteur défaut et si c'est MYISAM, alors les FK ne marcheront pas
 
 CREATE TABLE Genre(
     idGenre INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     nom VARCHAR(100) NOT NULL
     
     
-);
+    
+)ENGINE=InnoDB;
 CREATE TABLE Titre(
     idTitre INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    titre TEXT NOT NULL
+    titre TEXT NOT NULL,
+    idGenre INT NOT NULL,
+    foreign key (idGenre) REFERENCES genre(idGenre)
+    
 -- comment lier avec le genre ?
     
-);
+)ENGINE=InnoDB;
 
 CREATE TABLE AuTit(
     idTitre INT NOT NULL,
-    idGenre INT NOT NULL,
-    PRIMARY KEY (idGenre,idTitre) -- c'est pas une primary key qu'il faut mettre
+    idAuteur INT NOT NULL,
+    foreign key fk_TitAut(idTitre) REFERENCES Titre(idTitre),
+    foreign key(idAuteur) REFERENCES Auteur(idAuteur)
+     -- c'est pas une primary key qu'il faut mettre
     -- il manque les contraintes FK
-);
+)ENGINE=InnoDB;
