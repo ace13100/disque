975f3ad2a3ecdb7c7b81f535ee51ee3dd657b4f9
alosyus3 <40473689+alosyus3@users.noreply.github.com>
2021-05-18 11:06:24 +0200

    Corrections à effectuer

--- disque.sql
+++ disque.sql
@@ -1,11 +1,12 @@
-
+-- avant de faire le use music, il aurait fallu créer la BDD ici
 
 use music;
 CREATE TABLE Auteur(
     idAuteur INT NOT NULL AUTO_INCREMENT,
     nom TEXT NOT NULL,
     PRIMARY KEY (idAuteur)
-);
+); -- pense à mettre le moteur de chaque table, sinon il prend le moteur défaut et si c'est MYISAM, alors les FK ne marcheront pas
+
 CREATE TABLE Genre(
     idGenre INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     nom VARCHAR(100) NOT NULL
@@ -15,12 +16,13 @@ CREATE TABLE Genre(
 CREATE TABLE Titre(
     idTitre INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     titre TEXT NOT NULL
-
+-- comment lier avec le genre ?
     
 );
 
 CREATE TABLE AuTit(
     idTitre INT NOT NULL,
     idGenre INT NOT NULL,
-    PRIMARY KEY (idGenre,idTitre)
+    PRIMARY KEY (idGenre,idTitre) -- c'est pas une primary key qu'il faut mettre
+    -- il manque les contraintes FK
 );
