89107b06c8523d616154f10d93fd8e20273ac3cb
saadi <sadly4012@gmail.com>
2021-05-18 10:09:34 +0200

    add disque.sql corriger

--- disque.sql
+++ disque.sql
@@ -1,26 +1,26 @@
-CREATE DATABASE music;
+
+
 use music;
 CREATE TABLE Auteur(
-    idAuteur INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    nom TEXT NOT NULL ,
+    idAuteur INT NOT NULL AUTO_INCREMENT,
+    nom TEXT NOT NULL,
     PRIMARY KEY (idAuteur)
 );
 CREATE TABLE Genre(
     idGenre INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    nom VARCHAR(100) NOT NULL,
-    PRIMARY KEY (idGenre)
+    nom VARCHAR(100) NOT NULL
+    
     
 );
 CREATE TABLE Titre(
     idTitre INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    titre TEXT NOT NULL,
-    PRIMARY KEY (idGenre)
+    titre TEXT NOT NULL
+
     
 );
 
 CREATE TABLE AuTit(
-    idTitre INT PRIMARY KEY NOT NULL,
-    idGenre INT PRIMARY KEY NOT NULL,
+    idTitre INT NOT NULL,
+    idGenre INT NOT NULL,
     PRIMARY KEY (idGenre,idTitre)
-    
 );
